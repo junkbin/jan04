f05ebef4344dedc002541137af037b88be2a2ba8
santosh <santosh.ece06@gmail.com>
2019-01-08 10:14:29 +0530

    u

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
-    'Shake or presssss menu button for dev menu',
+    'Shake or presss menu button for dev menu',
 });
 
 type Props = {};
